b201429c5a28db899591665e84a080952c6dbbea
DebugConnoisseur <67976720+DebugConnoisseur@users.noreply.github.com>
2022-03-19 11:33:42 +0000

    Update helloworld.cs

--- tutorial/basics/00.Hello World Program/helloworld.cs
+++ tutorial/basics/00.Hello World Program/helloworld.cs
@@ -1,3 +1,9 @@
 //Console.WriteLine command is used for printing things on the console, here we are printing hello world
-Console.WriteLine("Hello World")
+//Every command in C# should end with a semicolon
+Console.WriteLine("Hello World");
+
+//You may notice that the console closes immediately and you may not be able to see the output so to counter this leave a Console.ReadLine command
+
+Console.ReadLine();
   
+//Now, the console stays still until you type in something in the console
